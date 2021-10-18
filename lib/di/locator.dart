6762e32c46ce5f6a7e9061c134897212d6d7c43d
ArindoGhosh <arindoghosh@deloitte.com>
2021-10-18 22:02:19 +0530

    added tests cases for UserLogCheckCubit

--- lib/di/locator.dart
+++ lib/di/locator.dart
@@ -4,6 +4,7 @@ import 'package:get_it/get_it.dart';
 import 'package:watch_my_stock/data/local/app_preference.dart';
 import 'package:watch_my_stock/data/repos/fake_auth_repo.dart';
 import 'package:watch_my_stock/data/repos/i_auth_repo_.dart';
+import 'package:watch_my_stock/domain/auth/i_user_component.dart';
 import 'package:watch_my_stock/domain/auth/user_auth_check/user_log_check_cubit.dart';
 import 'package:watch_my_stock/domain/auth/user_component.dart';
 import 'package:watch_my_stock/domain/auth/user_auth_cubit.dart';
@@ -13,7 +14,7 @@ final getIt = GetIt.instance;
 
 void init() {
   //reusable business logic components
-  getIt.registerLazySingleton(() => UserComponent(getIt()));
+  getIt.registerLazySingleton<IUserComponent>(() => UserComponent(getIt()));
   //bloc
   getIt.registerFactory(() => UserLogCheckCubit(getIt()));
   getIt.registerFactory(() => UserAuthCubit(getIt()));
